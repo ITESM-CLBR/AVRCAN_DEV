abab13e8bf41cf3bef024963a646a1eda6406282
Eric Sanchez <esanchez.briones@gmail.com>
2015-07-14 17:36:33 -0500

    Dummy Main
    Add Dummy Main

---
+++ Implementation/src/main.c
@@ -0,0 +1,46 @@
+/****************************************************************** 
+* main.c
+* Created on: Jul 14, 2015
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+*© 2015
+*****************************************************************/ 
+#ifndef main_C_
+ #define main_C_
+#endif
+/**
+ * \addtogroup main_module
+ * @{
+ */
+
+/******************************************************************/
+/*    I N C L U D E   F I L E S                                   */
+/******************************************************************/
+#include <cpu.h>
+
+
+/*******************************************************************************************/
+/*    M A C R O S                                                                          */
+/*******************************************************************************************/
+
+
+/*******************************************************************************************/
+/*    F U N C T I O N   P R O T O T Y P E S                                                */
+/*******************************************************************************************/
+int  main(void){
+
+
+
+	return 0;
+}
+/**
+ * @}
+ */
